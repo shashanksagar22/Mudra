d15852d69c6e1af7eb60f0e20f553b45cf23f3b2
shashank sagar <107057875+shashanksagar22@users.noreply.github.com>
2025-09-24 18:23:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mudra/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.mudra.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//val PurpleStart = Color(color:0xFFD24BE9)
+val PurpleStart : Color = Color(  0xFFD24BE9)
+val PurpleEnd : Color = Color( 0xFFDF8FEC)
+
+val BlueStart : Color = Color(  0xFF2196F3)
+val BlueEnd : Color = Color(  0xFF79C3FD)
+
+val OrangeStart : Color= Color( 0xFFFF8400)
+
+val OrangeEnd : Color= Color(  0xFFFDA35F)
+
+val GreenStart : Color = Color( 0xFF11B114)
+val GreenEnd : Color = Color( 0xFF52DB59)
