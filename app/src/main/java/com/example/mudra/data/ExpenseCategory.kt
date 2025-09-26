bf38e3cd7c4d2f21b670f83c2cc3d513feddd49a
shashank sagar <107057875+shashanksagar22@users.noreply.github.com>
2025-09-26 23:09:17 +0530

    .

--- app/src/main/java/com/example/mudra/data/ExpenseCategory.kt
+++ app/src/main/java/com/example/mudra/data/ExpenseCategory.kt
@@ -2,9 +2,8 @@ package com.example.mudra.data
 
 import androidx.compose.ui.graphics.Color
 
-
 data class ExpenseCategory(
-         val name: String,
-         val percentage: Float,
-         val color: Color
-     )
+    val name: String,
+    val percentage: Float,
+    val color: Color
+)
